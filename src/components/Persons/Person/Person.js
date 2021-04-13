bff02d978388af8c704294308cf41b191035fdb2
Aniketh <aniketh12@gmail.com>
2021-04-13 21:42:10 +0530

    remove StyledComponents from Person.js

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,29 +1,14 @@
 import React from 'react';
 import './Person.css';
-import styled from 'styled-components';
-
-const StyledComps = styled.div`
-    width: 30%;
-    margin: 20px auto;
-    border: 1px solid #eee;
-    box-shadow: 0 2px 3px #ccc;
-    padding: 16px;
-    text-align: center;
-
-    @media (min-width : 500px) {
-        width: 450px
-    }
-`;
+import classes from './Person.css';
 
 const person = (props) => {
     console.log('[Person.js] is rendering...')
     return (
-        <StyledComps>
-            <div className="Person">
+            <div className={classes.Person}>
             <input type="text" onChange={props.nameChanged} value={props.name}></input>
             <p onClick = {props.click}>I am {props.name} and I am {props.age} years old</p>
         </div>
-        </StyledComps>
     );
 };
 
