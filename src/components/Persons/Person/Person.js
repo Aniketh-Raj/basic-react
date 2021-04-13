251f379f22bc44ff141d17d22d7a4070e9ad39f3
Aniketh <aniketh12@gmail.com>
2021-04-13 22:19:16 +0530

    react class hooks - update hooks

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Person.css';
 import classes from './Person.css';
 
-const person = (props) => {
-    console.log('[Person.js] is rendering...')
-    return (
+class Person extends Component {
+
+    render() {
+        console.log('[Person.js] is rendering...')
+        return (
             <div className={classes.Person}>
-            <input type="text" onChange={props.nameChanged} value={props.name}></input>
-            <p onClick = {props.click}>I am {props.name} and I am {props.age} years old</p>
-        </div>
-    );
+                <input type="text" onChange={this.props.nameChanged} value={this.props.name}></input>
+                <p onClick={this.props.click}>I am {this.props.name} and I am {this.props.age} years old</p>
+            </div>
+        );
+    }
 };
 
-export default person;
+export default Person;
